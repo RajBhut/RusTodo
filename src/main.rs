280bb9fd603d8ac84c4ce311ab9f3597386ec3b5
RajBhut214 <rajbhut2832005@gmail.com>
2025-03-16 13:20:41 +0530

    add

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,17 @@ impl TODO {
             completed: false,
         }
     }
+    fn complete(&mut self) {
+        self.completed = true;
+    }
 }
 fn main() {
     let mut todo_list: Vec<TODO> = Vec::new();
     todo_list.push(TODO::new("Learning Rust".to_string()));
     todo_list.push(TODO::new("Trying to be Good".to_string()));
-
+    if let Some(task) = todo_list.get_mut(0) {
+        task.complete();
+    }
     for item in todo_list {
         print!("Task: {} , completed : {} \n", item.task, item.completed)
     }
